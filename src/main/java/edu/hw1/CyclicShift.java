e68ee8517497c7e6befa6e511addc3860f7a8407
Korepanov Alexander <sashakorepanov42@gmail.com>
2023-10-12 17:41:28 +0400

    Homework 1 last change bug

--- src/main/java/edu/hw1/CyclicShift.java
+++ src/main/java/edu/hw1/CyclicShift.java
@@ -4,7 +4,7 @@ public class CyclicShift {
     private CyclicShift() {
     }
 
-    private static int amountOfBinaryDigits(int n) {
+    private static int amountOfBinaryDigitsInNum(int n) {
         int ans = 0;
         int copy = n;
         while (copy > 0) {
@@ -15,13 +15,13 @@ public class CyclicShift {
     }
 
     public static int rotateLeft(int n, int shift) {
-        int bits = amountOfBinaryDigits(n);
+        int bits = amountOfBinaryDigitsInNum(n);
         int intMask = (1 << bits) - 1;
         return intMask & ((n << shift % bits) | (n >>> (bits - shift % bits)));
     }
 
     public static int rotateRight(int n, int shift) {
-        int bits = amountOfBinaryDigits(n);
+        int bits = amountOfBinaryDigitsInNum(n);
         int intMask = (1 << bits) - 1;
         return intMask & ((n << (bits - shift % bits)) | (n >>> shift % bits));
     }
