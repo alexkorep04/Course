cc61a87228ab4488c39d5a6b9b31969dea32f59f
Korepanov Alexander <sashakorepanov42@gmail.com>
2023-10-31 21:20:34 +0400

    Hw 5 change 7 task

--- src/main/java/edu/hw5/Task7.java
+++ src/main/java/edu/hw5/Task7.java
@@ -4,8 +4,18 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Task7 {
-    public static boolean isCorrectString(String input) {
-        Pattern pattern = Pattern.compile("^0[0|1]0$");
+    public static boolean isStringHasLengthMoreThanThreeAndThirdIsZero(String input) {
+        Pattern pattern = Pattern.compile("^[0|1][0|1]0[0,1]{0,}");
+        Matcher matcher = pattern.matcher(input);
+        return matcher.find();
+    }
+    public static boolean isStringHasEqualFirstAndLastSymbol(String input) {
+        Pattern pattern = Pattern.compile("^([0|1])[0|1]*\\1$");
+        Matcher matcher = pattern.matcher(input);
+        return matcher.find();
+    }
+    public static boolean isStringHasLengthFromOneToThree(String input) {
+        Pattern pattern = Pattern.compile("^[0|1]{1,3}$");
         Matcher matcher = pattern.matcher(input);
         return matcher.find();
     }
--- src/test/java/edu/hw5/Task7Test.java
+++ src/test/java/edu/hw5/Task7Test.java
@@ -6,25 +6,61 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class Task7Test {
     @Test
-    @DisplayName("Test correct string")
-    public void testCorrectString() {
-        boolean response = Task7.isCorrectString("010");
+    @DisplayName("Test string has equal first and last symbol")
+    public void testStringHasEqualFirstAndLastSymbol() {
+        boolean response = Task7.isStringHasEqualFirstAndLastSymbol("001100");
 
         assertThat(response).isTrue();
     }
     @Test
-    @DisplayName("Test uncorrect string with big length")
-    public void testUncorrectString() {
-        boolean response = Task7.isCorrectString("0101");
+    @DisplayName("Test string has not equal first and last symbol")
+    public void testStringHasNotEqualFirstAndLastSymbol() {
+        boolean response = Task7.isStringHasEqualFirstAndLastSymbol("001101");
+
+        assertThat(response).isFalse();
+    }
+
+    @Test
+    @DisplayName("Test string has equal first and last symbol, but has not alphabet symbols")
+    public void testStringHasEqualFirstAndLastSymbolButHasUncorrectSymbols() {
+        boolean response = Task7.isStringHasEqualFirstAndLastSymbol("101121");
 
         assertThat(response).isFalse();
     }
     @Test
-    @DisplayName("Test uncorrect string with uncorrect third symbol")
-    public void testUncorrectStringWithWrongThirdSymbol() {
-        boolean response = Task7.isCorrectString("011");
+    @DisplayName("Test string that has length three and third is zero")
+    public void testStringHasLengthThreeAndThirdIsZero() {
+        boolean response = Task7.isStringHasLengthMoreThanThreeAndThirdIsZero("100");
+
+        assertThat(response).isTrue();
+    }
+    @Test
+    @DisplayName("Test string that has length three and third is not zero")
+    public void testStringHasLengthThreeAndThirdIsNotZero() {
+        boolean response = Task7.isStringHasLengthMoreThanThreeAndThirdIsZero("101");
 
         assertThat(response).isFalse();
     }
+    @Test
+    @DisplayName("Test string that has not length more than three")
+    public void testStringHasNotLengthThree() {
+        boolean response = Task7.isStringHasLengthMoreThanThreeAndThirdIsZero("10");
 
+        assertThat(response).isFalse();
+    }
+    @Test
+    @DisplayName("Test string that has length from one to three")
+    public void testStringHasLengthFromOneToThree() {
+        boolean response = Task7.isStringHasLengthFromOneToThree("10");
+
+        assertThat(response).isTrue();
+    }
+
+    @Test
+    @DisplayName("Test string that has length from one to three")
+    public void testStringHasNotLengthFromOneToThree() {
+        boolean response = Task7.isStringHasLengthFromOneToThree("1011");
+
+        assertThat(response).isFalse();
+    }
 }
