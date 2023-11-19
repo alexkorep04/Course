616d7a2a03f277f59c28fdb7f0369efd3891d7ce
Korepanov Alexander <sashakorepanov42@gmail.com>
2023-11-19 11:38:31 +0400

    hw7 small change

--- src/test/java/edu/hw7/Task1Test.java
+++ src/test/java/edu/hw7/Task1Test.java
@@ -9,7 +9,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class Task1Test {
     IncrementClass incrementClass;
-
     @BeforeEach
     public void createObjects() {
         incrementClass = new IncrementClass();
