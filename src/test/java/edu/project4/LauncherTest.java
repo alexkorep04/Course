3d75c1ab8608a8a17ae8a2bbac7177fc6a8de898
Korepanov Alexander <sashakorepanov42@gmail.com>
2023-12-05 22:41:19 +0400

    project 4 add images of fractals

--- src/test/java/edu/project4/LauncherTest.java
+++ src/test/java/edu/project4/LauncherTest.java
@@ -1,9 +1,10 @@
 package edu.project4;
 
+import edu.project4.Functions.DiskTransformation;
 import edu.project4.Functions.HeartTransformation;
+import edu.project4.Functions.PolarTransformation;
 import edu.project4.Functions.SinusTransformation;
-import edu.project4.correcions.GammaCorrection;
-import edu.project4.entities.FractalImage;
+import edu.project4.Functions.SphereTransformation;
 import edu.project4.entities.ImageFormat;
 import edu.project4.entities.Rectangular;
 import edu.project4.launchers.Launcher;
@@ -19,7 +20,7 @@ public class LauncherTest {
     @DisplayName("Test of launcher")
     public void testGamma() {
         Assertions.assertDoesNotThrow(() ->
-            Launcher.generateFractalImage(1920, 1080, new MultiThreadRenderer(), new Rectangular(-4, -3, 8, 6), List.of(new HeartTransformation()),
+            Launcher.generateFractalImage(1920, 1080, new MultiThreadRenderer(), new Rectangular(-4, -3, 8, 6), List.of(new DiskTransformation()),
                 ImageFormat.JPEG, Path.of("1.jpeg")));
     }
 }
