c06e0d3d642254ea1569b0bf9ffd4a920f7a5e32
Korepanov Alexander <sashakorepanov42@gmail.com>
2023-12-09 15:05:17 +0400

    hw9 fix style

--- src/main/java/edu/hw9/Task1/Collector.java
+++ src/main/java/edu/hw9/Task1/Collector.java
@@ -5,8 +5,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Collector {
