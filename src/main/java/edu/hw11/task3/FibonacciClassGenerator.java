279cd3b11c16f34624e17a37922fed5becaf138a
Korepanov Alexander <sashakorepanov42@gmail.com>
2023-12-16 09:54:52 +0400

    hw11 add task3

--- src/main/java/edu/hw11/task3/FibonacciClassGenerator.java
+++ src/main/java/edu/hw11/task3/FibonacciClassGenerator.java
@@ -28,46 +28,40 @@ public class FibonacciClassGenerator {
         return dynamicType;
     }
 
-    public static class Fib implements ByteCodeAppender {
-        private final Label label;
-        int stackSize;
-        int localVarsAmount;
-
-        public Fib() {
-            this.label = new Label();
-            this.stackSize = 7;
-            this.localVarsAmount = 3;
-        }
+    private static class Fib implements ByteCodeAppender {
 
         @Override
+        @SuppressWarnings("MagicNumber")
         public Size apply(MethodVisitor mv, Implementation.@NotNull Context context, @NotNull MethodDescription md) {
+            Label label = new Label();
+            int stackSize = 5;
+            int localVarsAmount = 2;
             mv.visitCode();
+
             mv.visitVarInsn(Opcodes.ILOAD, 1);
             mv.visitInsn(Opcodes.ICONST_2);
             mv.visitJumpInsn(Opcodes.IF_ICMPGE, label);
-            returnValue(mv);
-            mv.visitLabel(label);
-            mv.visitFrame(Opcodes.F_SAME, 0, null, 0, null);
-            mv.visitVarInsn(Opcodes.ALOAD, 0);
-            subIConst(mv, Opcodes.ICONST_2);
-            invoke(mv, md);
-            mv.visitVarInsn(Opcodes.ALOAD, 0);
-            subIConst(mv, Opcodes.ICONST_1);
-            invoke(mv, md);
-            mv.visitInsn(Opcodes.LADD);
-            mv.visitInsn(Opcodes.LRETURN);
-            mv.visitMaxs(stackSize, localVarsAmount);
-            mv.visitEnd();
-            return new ByteCodeAppender.Size(stackSize, localVarsAmount);
-        }
 
-        private void returnValue(MethodVisitor mv) {
             mv.visitVarInsn(Opcodes.ILOAD, 1);
             mv.visitInsn(Opcodes.I2L);
             mv.visitInsn(Opcodes.LRETURN);
-        }
 
-        private void invoke(MethodVisitor mv, MethodDescription md) {
+            mv.visitLabel(label);
+            mv.visitFrame(Opcodes.F_SAME, 0, null, 0, null);
+            mv.visitVarInsn(Opcodes.ALOAD, 0);
+            mv.visitVarInsn(Opcodes.ILOAD, 1);
+            mv.visitInsn(Opcodes.ICONST_1);
+            mv.visitInsn(Opcodes.ISUB);
+            mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL,
+                md.getDeclaringType().getTypeName(),
+                md.getName(),
+                md.getDescriptor(),
+                false
+            );
+            mv.visitVarInsn(Opcodes.ALOAD, 0);
+            mv.visitVarInsn(Opcodes.ILOAD, 1);
+            mv.visitInsn(Opcodes.ICONST_2);
+            mv.visitInsn(Opcodes.ISUB);
             mv.visitMethodInsn(
                 Opcodes.INVOKEVIRTUAL,
                 md.getDeclaringType().getTypeName(),
@@ -75,12 +69,10 @@ public class FibonacciClassGenerator {
                 md.getDescriptor(),
                 false
             );
-        }
-
-        private void subIConst(MethodVisitor mv, int opcode) {
-            mv.visitVarInsn(Opcodes.ILOAD, 1);
-            mv.visitInsn(opcode);
-            mv.visitInsn(Opcodes.ISUB);
+            mv.visitInsn(Opcodes.LADD);
+            mv.visitInsn(Opcodes.LRETURN);
+            mv.visitEnd();
+            return new ByteCodeAppender.Size(stackSize, localVarsAmount);
         }
     }
 }
